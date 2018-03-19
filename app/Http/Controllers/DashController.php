6b4f3b8c4cb3a84b9bcf0f682286c35aa02c52dd
Kauã Semenov <kauagabrielsemenov@gmail.com>
2018-03-19 01:05:14 -0300

    Implementações de layout do menu

---
+++ app/Http/Controllers/DashController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace HelpNow\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DashController extends Controller
+{
+
+    public function index()
+    {
+        return view('HelpNow/DashController/dash');
+    }
+
+
+    public function create()
+    {
+        //
+    }
+
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+
+    public function show($id)
+    {
+        //
+    }
+
+
+    public function edit($id)
+    {
+        //
+    }
+
+
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ resources/views/helpnow/HomeController/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('conteudo')
+        <div class="full-height flex-center position-ref">
+            <div class="content">
+                <div class="titulo m-b-md">
+                    Help Now
+                </div>
+            </div>
+        </div>
+@endsection
